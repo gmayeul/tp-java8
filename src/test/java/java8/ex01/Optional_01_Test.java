a8228be583b96488eeef169c721c8473292ce29a
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-06-14 14:30:19 +0200

    ex1 des Optional parce qu'au fond on s'en moque un peu des Optional

--- src/test/java/java8/ex01/Optional_01_Test.java
+++ src/test/java/java8/ex01/Optional_01_Test.java
@@ -16,86 +16,130 @@ import static org.junit.Assert.*;
  */
 public class Optional_01_Test {
 
-    class NotFountException extends RuntimeException {}
+	class NotFoundException extends RuntimeException {
 
+		/**
+		 * 
+		 */
+		public NotFoundException() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
 
-    // tag::findMethod[]
-    <T> T find(List<T> list, Predicate<T> predicate) {
-        T result = null;
+		/**
+		 * @param message
+		 * @param cause
+		 * @param enableSuppression
+		 * @param writableStackTrace
+		 */
+		public NotFoundException(String message, Throwable cause, boolean enableSuppression,
+				boolean writableStackTrace) {
+			super(message, cause, enableSuppression, writableStackTrace);
+			// TODO Auto-generated constructor stub
+		}
 
-        for (T p : list) {
-            if (predicate.test(p)) {
-                result = p;
-                break;
-            }
-        }
+		/**
+		 * @param message
+		 * @param cause
+		 */
+		public NotFoundException(String message, Throwable cause) {
+			super(message, cause);
+			// TODO Auto-generated constructor stub
+		}
 
-        return result;
-    }
-    // end::findMethod[]
+		/**
+		 * @param message
+		 */
+		public NotFoundException(String message) {
+			super(message);
+			// TODO Auto-generated constructor stub
+		}
 
+		/**
+		 * @param cause
+		 */
+		public NotFoundException(Throwable cause) {
+			super(cause);
+			// TODO Auto-generated constructor stub
+		}
+	}
 
-    @Test
-    public void test_optional_found() throws Exception {
+	// tag::findMethod[]
+	<T> T find(List<T> list, Predicate<T> predicate) {
+		T result = null;
 
-        List<Person> personList = Data.buildPersonList(100);
+		for (T p : list) {
+			if (predicate.test(p)) {
+				result = p;
+				break;
+			}
+		}
 
-        // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
-        // TODO age == 10
-        Optional<Person> result = null;
+		return result;
+	}
+	// end::findMethod[]
 
-        assertThat(result, instanceOf(Optional.class));
-        assertThat(result.isPresent(), is(true));
-        assertThat(result.get(), instanceOf(Person.class));
-        assertThat(result.get(), hasProperty("firstname", is("first_10")));
-        assertThat(result.get(), hasProperty("age", is(10)));
-    }
+	@Test
+	public void test_optional_found() throws Exception {
 
-    @Test
-    public void test_optional_notfound() throws Exception {
+		List<Person> personList = Data.buildPersonList(100);
 
+		// TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
+		// TODO age == 10
+		Optional<Person> result = Optional.of(find(personList, p -> p.getAge() == 10));
 
-        List<Person> personList = Data.buildPersonList(100);
+		assertThat(result, instanceOf(Optional.class));
+		assertThat(result.isPresent(), is(true));
+		assertThat(result.get(), instanceOf(Person.class));
+		assertThat(result.get(), hasProperty("firstname", is("first_10")));
+		assertThat(result.get(), hasProperty("age", is(10)));
+	}
 
-        // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
-        // TODO age == 400
-        Optional<Person> result = null;
+	@Test
+	public void test_optional_notfound() throws Exception {
 
-        assertThat(result, instanceOf(Optional.class));
-        assertThat(result.isPresent(), is(false));
-    }
+		List<Person> personList = Data.buildPersonList(100);
 
-    @Test(expected = NotFountException.class)
-    public void test_optional_notfound_throw_exception() throws Exception {
+		// TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
+		// TODO age == 400
+		Optional<Person> result = Optional.ofNullable(find(personList, p -> p.getAge() == 400));
 
+		assertThat(result, instanceOf(Optional.class));
+		assertThat(result.isPresent(), is(false));
+	}
 
-        List<Person> personList = Data.buildPersonList(100);
+	@Test(expected = NotFoundException.class)
+	public void test_optional_notfound_throw_exception() throws Exception {
 
-        // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
-        // TODO age == 10 et firstname == "last_10"
-        Optional<Person> result = null;
+		List<Person> personList = Data.buildPersonList(100);
 
-        // TODO Utiliser la méthode orElseThrow pour déclencher l'exception NotFountException si non trouvé
-    }
+		// TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
+		// TODO age == 10 et firstname == "last_10"
+		Optional<Person> result = Optional
+				.ofNullable(find(personList, p -> p.getAge() == 10 && p.getLastname().equals("last")));
 
-    @Test
-    public void test_optional_notfound_with_default_value() throws Exception {
+		// TODO Utiliser la méthode orElseThrow pour déclencher l'exception
+		// NotFoundException
+		result.orElseThrow(() -> new NotFoundException("Aucune personne ne répond à ces critères"));
+	}
 
+	@Test
+	public void test_optional_notfound_with_default_value() throws Exception {
 
-        List<Person> personList = Data.buildPersonList(100);
+		List<Person> personList = Data.buildPersonList(100);
 
-        Person defaultValue = new Person();
-        defaultValue.setFirstname("DEFAULT");
-        defaultValue.setLastname("DEFAULT");
+		Person defaultValue = new Person();
+		defaultValue.setFirstname("DEFAULT");
+		defaultValue.setLastname("DEFAULT");
 
-        // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate, T defaultValue)
-        // TODO predicate => age == 400
-        Person result = null;
+		// TODO invoquer la méthode find(List<T> list, Predicate<T> predicate, T
+		// defaultValue)
+		// TODO predicate => age == 400
+		Person result = null;
 
-        assertThat(result, notNullValue());
-        assertThat(result, hasProperty("firstname", is("DEFAULT")));
-        assertThat(result, hasProperty("lastname", is("DEFAULT")));
-    }
-
+		assertThat(result, notNullValue());
+		assertThat(result, hasProperty("firstname", is("DEFAULT")));
+		assertThat(result, hasProperty("lastname", is("DEFAULT")));
+	}
 
 }
